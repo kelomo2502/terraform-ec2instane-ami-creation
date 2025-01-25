9ee76a97113edbba83ac80d71cf957d099804e50
oyewunmi gbenga kelvin <kelvinoye@gmail.com>
2025-01-25 22:38:53 +0100

    add ami and variables

--- instance_state.tf
+++ instance_state.tf
@@ -1,4 +1,5 @@
-resource "aws_ec2_instance_state" "web-state" {
-  instance_id = aws_instance.web.id
+resource "aws_ec2_instance_state" "web_state" {
+  count       = length(aws_instance.web)
+  instance_id = aws_instance.web[count.index].id
   state       = "running"
 }
--- variable.tf
+++ variable.tf
@@ -7,3 +7,18 @@ variable "availability_zone" {
   type    = string
   default = "us-east-1a"
 }
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "ec2_instance_name" {
+  type    = string
+  default = "Dev_Server"
+}
+
+variable "number_of_instances" {
+  type    = number
+  default = 1
+}
