922b79ea01c78da09fb40c6a062f686cd5f2b55d
oyewunmi gbenga kelvin <kelvinoye@gmail.com>
2025-01-25 22:38:53 +0100

    add ami and variables

--- instance.tf
+++ instance.tf
@@ -1,16 +1,29 @@
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.amazon_linux.id
-  instance_type          = "t2.micro"
+  instance_type          = var.instance_type
   key_name               = aws_key_pair.gb_key.key_name
   vpc_security_group_ids = [aws_security_group.gb_sg.id]
   availability_zone      = var.availability_zone
+  count                  = var.number_of_instances
 
   tags = {
-    Name = "kelomoServer"
+    Name = var.ec2_instance_name
   }
+
+
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+              EOF
+
+
 }
 
 output "instance_public_ip_addr" {
-  value = aws_instance.web.public_ip
+  value = [for instance in aws_instance.web : instance.public_ip]
 }
-
